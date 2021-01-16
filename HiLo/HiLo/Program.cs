723ced53252b1d5c95529fd8af36dd5bec293450
NotCreative21 <61510083+NotCreative21@users.noreply.github.com>
2021-01-16 06:51:04 -0500

    initial commit

---
+++ HiLo/HiLo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace HiLo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hilo");
+            Console.WriteLine("I'm thinking of a number in the range [1, 100].");
+            Console.WriteLine("Can you guess it?");
+
+            Console.WriteLine("Choose difficulty:");
+            Console.WriteLine("(1) Easy (8 Guesses)");
+            Console.WriteLine("(2) Medium (7 Guesses)");
+            Console.WriteLine("(3) Hard (6 Guesses)");
+
+            Random random = new Random();
+
+            int number = random.Next(1, 100);
+            int guesslimit = 0;
+            int gamemode = Convert.ToInt32(Console.ReadLine());
+            switch (gamemode) {
+                case 1:
+                    Console.WriteLine("You chose: (1) Easy (8 Guesses)");
+                    guesslimit = 7;
+                    break;
+                case 2:
+                    Console.WriteLine("You chose: (2) Medium (7 Guesses)");
+                    guesslimit = 6;
+                    break;
+                case 3:
+                    Console.WriteLine("You chose: (3) Hard (6 Guesses)");
+                    guesslimit = 5;
+                    break;
+                default:
+                    Console.WriteLine("Please state a valid gamemode");
+                    break;
+            }
+            while (guesslimit != 0)
+            {
+                Console.WriteLine("What's your guess? ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == number)
+                {
+                    Console.WriteLine("I chose the number " + number);
+                    Console.WriteLine("~~ Victory ~~");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower!");
+                    Console.WriteLine("Remaining guesses: " + guesslimit);
+                    guesslimit--;
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher!");
+                    Console.WriteLine("Remaining guesses: " + guesslimit);
+                    guesslimit--;
+                }
+            }
+
+            Console.WriteLine("I chose the number " + number);
+            Console.WriteLine("-- Defeat --");
+
+        }
+    }
+}
---
+++ luckysevens/luckysevens/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime;
+
+namespace luckysevens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int roll = 0;
+            Console.WriteLine("Welcome to Lucky Sevens!");
+            Console.Write("Please enter a starting amount of money: $");
+            double startcash = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("starting cash: $" + startcash);
+            while (startcash > 0)
+            {
+                Console.Write("Please place a bet: $");
+                double betd = Convert.ToDouble(Console.ReadLine());
+                if (betd > 0 && betd <= startcash)
+                {
+                    Console.WriteLine("You bet: $" + betd);
+                    if (betd < startcash)
+                    {
+                        roll++;
+                        System.Random random = new System.Random();
+                        var dice1 = random.Next(1, 7);
+                        var dice2 = random.Next(1, 7);
+                        System.Console.Write("[" + dice1 + "]");
+                        System.Console.WriteLine("[" + dice2 + "]");
+                        if (dice1 + dice2 == 7)
+                        {
+                            Console.WriteLine("~~ Lucky Sevens! ~~");
+                            Console.WriteLine("You win: $" + (betd * 5));
+                            double win = betd * 5;
+                            startcash = win + startcash;
+                        }
+                        //reroll
+                        else if (dice1 == dice2 && dice1 + dice2 != 7)
+                        {
+                            Console.WriteLine("~ Reroll ~");
+                            Console.WriteLine("You win: $" + betd);
+                            Console.WriteLine("Monkey: $" + startcash);
+                            roll++;
+                        }
+                        else
+                        {
+                            startcash = startcash - betd;
+                            Console.WriteLine("Monkey: $" + startcash);
+                        }
+                        Console.WriteLine("Do you wish to keep playing? (0) No (1) Yes:");
+                        int keepplay = Convert.ToInt32(Console.ReadLine());
+                        if (keepplay == 0)
+                        {
+                            Console.WriteLine("Thanks for playing!");
+                            Console.WriteLine("You quit after " + roll + " rolls!");
+                            Console.ReadLine();
+                            Environment.Exit(0);
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("You went broke after " + roll + " rolls!");
+                        Console.WriteLine("Thanks for playing!");
+                        Console.ReadLine();
+                        Environment.Exit(0);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid bet: $" + betd);
+                }
+
+            }
+        }
+    }
+}
